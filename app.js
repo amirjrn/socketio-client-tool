673d971a1019cea05b7de05482c0ea33ee72293c
Amir Jiryan <amirjiryan@gmail.comgit config --global user.email amirjiryan@gmail.comgit config --global user.email amirjiryan@gmail.com>
2020-05-27 20:54:39 +0430

    added support for multiple arguments entry

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 'use strict';
 
-let eventsToListen = ['message'];
-let socket = {};
-let disconnectedInterval = null;
-let connectionTimeout = null;
-let title = document.title;
-let localdb = null;
+var eventsToListen = ['message'];
+var socket = {};
+var disconnectedInerval;
+var url = '';
+var options = '';
+var title = document.title;
+var localdb = null;
 
-const form = {
-  url: '',
-  path: '',
-  options: null,
-};
-
-const CONNECTION_TIMEOUT_MS = 10 * 1000;
-
-$(function() {
+$(function () {
   $('#jsonData').hide();
   $('.emitted-msg').hide();
   $('.emitted-failure-msg').hide();
@@ -23,7 +16,7 @@ $(function() {
   $('.listen-added-msg').hide();
   $('.disconnected-alert, .connected-alert').hide();
   $('#eventPanels').prepend(makePanel('message'));
-  $('input[type=radio][name=emitAs]').change(function() {
+  $('input[type=radio][name=emitAs]').change(function () {
     if (this.value === 'JSON') {
       $('#plainTextData').hide();
       $('#jsonData').show();
@@ -34,97 +27,56 @@ $(function() {
     }
   });
 
-
-  $("#connect").on('submit',function(e) {
-    const connectButton = $("#connect .btn");
+  $("#connect").submit(function (e) {
     e.preventDefault();
-
-    if (connectButton.html().trim() === 'Connect') {
-      form.url = $("#socketUrl").val().trim();
-      form.path = $("#socketPath").val().trim();
-      form.options = $("#socketOptions").val().trim();
-
-      try {
-        form.options = (form.options ? JSON.parse(form.options) : null);
-      } catch (err) {
-        alert('Invalid JSON options: ' + err.message);
-        return;
-      }
-
-      if (form.url === '') {
-        alert('Invalid URL given');
-        return;
-      }
-
-      const socketOptions = $.extend({}, {path: form.path}, form.options, {
-        transports: ['websocket']
-      });
-
-      console.log('Connecting to: ' + form.url + ' with options: ' + JSON.stringify(socketOptions));
-
-      setFormInputs(true, true);
-      socket = io(form.url, socketOptions);
-
-      connectionTimeout = setTimeout(function() {
-        socket.close();
-        socket = null;
-
-        alert('Connection to ' + form.url + ' timed out!');
-        setFormInputs(false, false);
-      }, CONNECTION_TIMEOUT_MS);
-
-      socket.on('connect', function () {
-        clearTimeout(connectionTimeout);
-        setFormInputs(true, false);
-        setSocketDetails(socket);
-
+    if ($("#connect .btn").html().trim() === 'Connect') {
+      url = $("#connect input:first").val().trim();
+      options = $("#connect_options").val().trim();
+      var opt = options ? JSON.parse(options) : null;
+      if (url === '') {
+        console.error('Invalid URL given');
+      } else {
+        socket = io(url, $.extend({}, opt, {
+          transports: ['websocket']
+        }));
         setHash();
-
-        $("#submitEmit").prop('disabled', false);
-        clearInterval(disconnectedInterval);
-        document.title = title;
-        $('.disconnected-alert').hide();
-        $('.connected-alert').show().delay(5000).fadeOut(1000);
-        $("#connectionPanel").prepend('<p><span class="text-muted">' + getFormattedNowTime() + '</span> Connected</p>');
-
-        connectButton.html('Disconnect');
-        connectButton.removeClass('btn-success');
-        connectButton.addClass('btn-danger');
-      });
-
-      socket.on('disconnect', function (sock) {
-        socket = null;
-
-        $("#submitEmit").prop('disabled', true);
-        setFormInputs(false, false);
-        setSocketDetails(null);
-
-        $('#socketInfo span').text('');
-
-        disconnectedInterval = setInterval(function () {
-          if (document.title === "Disconnected") {
-            document.title = title;
-          } else {
-            document.title = "Disconnected";
-          }
-        }, 800);
-
-        $('.disconnected-alert').hide();
-        $('.disconnected-alert').show();
-        $("#connectionPanel").prepend('<p><span class="text-muted">' + getFormattedNowTime() + '</span> Disconnected --> ' + sock + '</p>');
-        connectButton.html('Connect');
-        connectButton.removeClass('btn-danger');
-        connectButton.addClass('btn-success');
-      });
-
-      registerEvents();
+        socket.on('connect', function () {
+          $("#submitEmit").prop('disabled', false);
+          clearInterval(disconnectedInerval);
+          document.title = title;
+          $('.disconnected-alert').hide();
+          $('.connected-alert').show().delay(5000).fadeOut(1000);
+          $("#connectionPanel").prepend('<p><span class="text-muted">' + getFormattedNowTime() + '</span> Connected</p>');
+          $("#connect .btn").html('Disconnect');
+          $("#connect .btn").removeClass('btn-success');
+          $("#connect .btn").addClass('btn-danger');
+          $("#connect input").prop('disabled', true);
+        });
+        socket.on('disconnect', function (sock) {
+          $("#submitEmit").prop('disabled', true);
+          disconnectedInerval = setInterval(function () {
+            if (document.title === "Disconnected") {
+              document.title = title;
+            } else {
+              document.title = "Disconnected";
+            }
+          }, 800);
+          $('.disconnected-alert').hide();
+          $('.disconnected-alert').show();
+          $("#connectionPanel").prepend('<p><span class="text-muted">' + getFormattedNowTime() + '</span> Disconnected --> ' + sock + '</p>');
+          $("#connect .btn").html('Connect');
+          $("#connect .btn").removeClass('btn-danger');
+          $("#connect .btn").addClass('btn-success');
+          $("#connect input").prop('disabled', false);
+        });
+        registerEvents();
+      }
     } else {
       socket.disconnect();
     }
   });
 
-
-  $("#addListener").submit(function(e) {
+  $("#addListener").submit(function (e) {
     e.preventDefault();
     var event = $("#addListener input:first").val().trim();
     if (event.length !== 0 && eventsToListen.indexOf(event) === -1) {
@@ -133,7 +85,6 @@ $(function() {
       $("#addListener input:first").val('');
       setHash();
       registerEvents();
-
       $('.listen-added-msg').show().delay(1000).fadeOut(1000);
     } else {
       $('.listen-failure-msg').show().delay(1500).fadeOut(1000);
@@ -141,20 +92,19 @@ $(function() {
     }
   });
 
-
-  $("#emitData").submit(function(e) {
-    if (socket && socket.io) {
+  $("#emitData").submit(function (e) {
+    if (socket.io) {
       var event = $("#emitData #event-name").val().trim();
-      var data;
+      var data = [];
       if ($('#emitAsJSON').is(":checked")) {
         data = parseJSONForm();
       }
       if ($('#emitAsPlaintext').is(":checked")) {
         var text = $("#emitData #data-text").val().trim();
         try {
-          data = JSON.parse(text);
+          data = JSON.parse(text).split(",");
         } catch (ex) {
-          data = text;
+          data = text.split(",");
         }
       }
       if (event !== '' && data !== '') {
@@ -165,6 +115,7 @@ $(function() {
         };
         postDataIntoDB(emitData);
         addHistoryPanel(emitData);
+
         emit(event, data, 'emitAck-' + event);
         $('.emitted-msg').show().delay(700).fadeOut(1000)
       } else {
@@ -177,21 +128,19 @@ $(function() {
     e.preventDefault();
   });
 
-
-  $("#addNewJsonField").click(function(e) {
+  $("#addNewJsonField").click(function (e) {
     e.preventDefault();
     var template = "<div class=\"form-inline\"><div class=\"form-group\"><input type=\"text\" class=\"form-control key\"></div> <div class=\"form-group\"><input type=\"text\" class=\"form-control value\"></div> <div class=\"form-group\"><button class=\"btn btn-xs btn-danger remove\" type=\"button\">remove</button></div></div>";
     $("#jsonData").append(template);
   });
 
-
-  $("#jsonData").on('click', '.remove', function(e) {
+  $("#jsonData").on('click', '.remove', function (e) {
     e.preventDefault();
     $(this).closest(".form-inline").remove();
   });
 
 
-  $("#clearHistory").on('submit',function(e) {
+  $("#clearHistory").submit(function (e) {
     e.preventDefault();
     initDB(true);
     $('#emitHistoryPanels').empty();
@@ -202,65 +151,40 @@ $(function() {
   initHistory();
 });
 
-function setFormInputs(disableForm, disableSubmit) {
-  $("#connect input").prop('disabled', disableForm);
-  $("#connect .btn").prop('disabled', disableSubmit);
-}
-
-function setSocketDetails(socket) {
-  const socketInfo = $('#socketInfo');
-
-  if (socket) {
-    socketInfo.show();
-    $('#socketInfo .well .details').append(`<div>Id: ${socket.id}</div><div>Connected: ${new Date()}</div>`);
-  } else {
-    socketInfo.hide();
-    $('#socketInfo .well .details').html('');
-  }
-}
-
 function setHash() {
-  if (form.url !== '' && eventsToListen.length > 0) {
-    const hashEvents = eventsToListen.slice();
-    const messageIndex = hashEvents.indexOf('message');
-
+  if (url !== '' && eventsToListen.length > 0) {
+    var hashEvents = eventsToListen.slice();
+    var messageIndex = hashEvents.indexOf('message');
     if (messageIndex !== -1) {
       hashEvents.splice(messageIndex, 1);
     }
-
-    location.hash = 'url=' + window.btoa(form.url) + '&path=' + window.btoa(form.path) + '&opt=' + window.btoa(form.options ? JSON.stringify(form.options) : '') + '&events=' + hashEvents.join();
+    location.hash = "url=" + window.btoa(url) + "&opt=" + window.btoa(options) + "&events=" + hashEvents.join();
   }
 }
 
 function processHash() {
-  let hash = location.hash.substr(1);
+  var hash = location.hash.substr(1);
   hash = decodeURI(hash);
-
   if (hash.indexOf('url=') !== -1 && hash.indexOf('events=') !== -1) {
-    const hashUrl = window.atob(hash.substr(hash.indexOf('url=')).split('&')[0].split('=')[1]);
-    const hashPath = window.atob(hash.substr(hash.indexOf('path=')).split('&')[0].split('=')[1]);
-    const hashOpt = window.atob(hash.substr(hash.indexOf('opt=')).split('&')[0].split('=')[1]);
-    const hashEvents = hash.substr(hash.indexOf('events=')).split('&')[0].split('=')[1].split(',');
-
+    var hashUrl = window.atob(hash.substr(hash.indexOf('url=')).split('&')[0].split('=')[1]);
+    var hashOpt = window.atob(hash.substr(hash.indexOf('opt=')).split('&')[0].split('=')[1]);
+    var hashEvents = hash.substr(hash.indexOf('events=')).split('&')[0].split('=')[1].split(',');
     $.merge(eventsToListen, hashEvents);
-    $.each(hashEvents, function(index, value) {
+    $.each(hashEvents, function (index, value) {
       if (value !== '') {
         $('#eventPanels').prepend(makePanel(value));
       }
     });
-
-    $('#socketUrl').val(hashUrl);
-    $('#socketPath').val(hashPath);
-    $('#socketOptions').val(hashOpt);
-
+    $('#connect input:first').val(hashUrl);
+    $('#connect_options').val(hashOpt);
     $('#connect').submit();
   }
 }
 
 function registerEvents() {
-  if (socket && socket.io) {
-    $.each(eventsToListen, function(index, value) {
-      socket.on(value, function(data) {
+  if (socket.io) {
+    $.each(eventsToListen, function (index, value) {
+      socket.on(value, function (data) {
         if (!data) {
           data = '-- NO DATA --'
         }
@@ -281,7 +205,7 @@ function clearEvents(event) {
 
 function clearAllEvents() {
   $('#eventPanels').find('.panel-body')
-    .each(function() {
+    .each(function () {
       $(this).empty();
     });
 }
@@ -289,7 +213,7 @@ function clearAllEvents() {
 function parseJSONForm() {
   var result = "{";
   var formInputs = $('#jsonData').find('.form-inline');
-  formInputs.each(function(index, el) {
+  formInputs.each(function (index, el) {
     var key = $(el).find('.key').val().trim();
     if (!key.length) {
       return true;
@@ -329,7 +253,7 @@ function getFormattedNowTime() {
 function initDB(clear) {
   var dbName = 'socketioClientDB';
   if (clear) {
-    localdb.destroy().then(function() {
+    localdb.destroy().then(function () {
       localdb = new PouchDB(dbName);
     });
   } else {
@@ -349,22 +273,22 @@ function initHistory() {
       }
     }
   };
-  localdb.put(ddoc).catch(function(err) {
+  localdb.put(ddoc).catch(function (err) {
     if (err.name !== 'conflict') {
       throw err;
     }
-  }).then(function() {
+  }).then(function () {
     return localdb.query('index', {
       descending: true,
       limit: 20
     });
-  }).then(function(result) {
+  }).then(function (result) {
     var rows = result.rows;
     for (var i in rows) {
       var history = rows[i].value;
       addHistoryPanel(history);
     }
-  }).catch(function(err) {
+  }).catch(function (err) {
     console.log(err);
   });
 }
@@ -378,19 +302,17 @@ function postDataIntoDB(data, callback) {
 
 function emit(event, data, panelId) {
   console.log('Emitter - emitted: ' + data);
-  const panel = $("#emitAckResPanels").find("[data-windowId='" + panelId + "']");
-  if (panel.length === 0) {
+  var panel = $("#emitAckResPanels").find("[data-windowId='" + panelId + "']");
+  if (panel.length == 0) {
     $('#emitAckResPanels').prepend(makePanel(panelId));
   }
-
-  const emitData = {
+  var emitData = {
     event: event,
     request: data,
     time: getFormattedNowTime()
   };
-
-  socket.emit(event, data, function(res) {
-    const elementToExtend = $("#emitAckResPanels").find("[data-windowId='" + panelId + "']");
+  socket.emit(event, ...data, function (res) {
+    var elementToExtend = $("#emitAckResPanels").find("[data-windowId='" + panelId + "']");
     elementToExtend.prepend('<p><span class="text-muted">' + getFormattedNowTime() + '</span><strong> ' + JSON.stringify(res) + '</strong></p>');
   });
 }
@@ -398,7 +320,7 @@ function emit(event, data, panelId) {
 function addHistoryPanel(history) {
   var histPanelId = history.event;
   var panel = $("#emitHistoryPanels").find("[data-windowId='" + histPanelId + "']");
-  if (panel.length === 0) {
+  if (panel.length == 0) {
     $('#emitHistoryPanels').prepend(makePanel(histPanelId));
   }
   var elementToExtend = $("#emitHistoryPanels").find("[data-windowId='" + histPanelId + "']");
@@ -409,10 +331,10 @@ function addHistoryPanel(history) {
   historyContent = historyContent.split('[[reqData]]').join(JSON.stringify(history.request));
   historyContent = historyContent.split('[[event]]').join(history.event);
   elementToExtend.prepend(historyContent);
-  $("#form" + id).submit(function(e) {
+  $("#form" + id).submit(function (e) {
     e.preventDefault();
     var id = $(this).find('[name="historyId"]').val();
-    var data = JSON.parse($(this).find('[name="reqData"]').val());
+    var data = [JSON.parse($(this).find('[name="reqData"]').val())];
     var event = $(this).find('[name="event"]').val();
     if (socket.io) {
       if (event !== '' && data !== '') {
